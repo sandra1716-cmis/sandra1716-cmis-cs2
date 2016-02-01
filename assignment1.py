16e95882ac772ce386f1fb4ad0b518bacfc58387
cmis <cmis@cmis-hs09.(none)>
2016-02-01 09:55:39 +0700

    made python script

---
+++ assignment1.py
@@ -0,0 +1,27 @@
+myname = "Sandra" 
+print myname
+myageindecimals = 16.9
+print myageindecimals
+myheightinmeters = 1.62
+print myheightinmeters
+onesideofsquare = 2
+print onesideofsquare
+lengthofrectangle = 4
+print lengthofrectangle
+heightofrectangle = 5
+print heightofrectangle
+oneyearinmonths = 12
+myage = 16
+myageinmonths = oneyearinmonths * myage
+print myageinmonths
+averagefemalelifeexpectancy = 71
+myage =16
+yearstolive = averagefemalelifeexpectancy - myage
+print  yearstolive
+onemeterinfeet = 3.28084
+myheightinfeet = myheightinmeters * onemeterinfeet
+print myheightinfeet
+averagefemaleheightthailand = 157.3
+myheight = 162
+differenceofmyheightandaverageheight = myheight - averagefemaleheightthailand
+print differenceofmyheightandaverageheight
