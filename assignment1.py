b8286921845fbce09f4e299b96b05d44bf14bf1d
cmis <cmis@cmis-hs09.(none)>
2016-02-02 11:51:53 +0700

    finished printing variables

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,4 @@
+#creatd a variable that defined my name
 myname = "Sandra" 
 print myname
 myageindecimals = 16.9
@@ -21,7 +22,18 @@ print  yearstolive
 onemeterinfeet = 3.28084
 myheightinfeet = myheightinmeters * onemeterinfeet
 print myheightinfeet
+
 averagefemaleheightthailand = 157.3
 myheight = 162
 differenceofmyheightandaverageheight = myheight - averagefemaleheightthailand
 print differenceofmyheightandaverageheight
+areaofsquare = onesideofsquare * onesideofsquare
+print areaofsquare
+volumeofhalfacube = (onesideofsquare ** 3 )/2
+print volumeofhalfacube
+oneninethareaofrectangle = (heightofrectangle * lengthofrectangle) * (1.0/9)
+print oneninethareaofrectangle
+print "Hello, people." + "My name is" + " " + myname + "." + "I am" + " " + str(myage) + "years old." + "I am" + " " + str(myageinmonths) + " " + "months old." + "I have approximately" + " " + str(yearstolive) + " " + "years to live." + "I am" + " " + str(myheight) + " " + "centimeters tall."
+print "Hi there","i have a square and a rectangle.","The square has a side of",str(onesideofsquare),"units.","Its area is",str(areaofsquare),"units squared.","The volume of half the square is",str(volumeofhalfacube),"units cubed.","The rectangle has a lenth of",str(lengthofrectangle),"and height of",str(heightofrectangle),"units."
+winkyface = ";)" 
+print winkyface * int(10000)
