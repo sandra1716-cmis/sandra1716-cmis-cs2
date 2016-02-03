2ae4a9d027675c331c3bfef3a9b99d20ff7fc50f
cmis <cmis@cmis-hs09.(none)>
2016-02-03 14:17:41 +0700

    added comments to the assignment

--- assignment1.py
+++ assignment1.py
@@ -1,38 +1,23 @@
-#creatd a variable that defined my name
-myname = "Sandra" 
-print myname
-myageindecimals = 16.9
-print myageindecimals
-myheightinmeters = 1.62
-print myheightinmeters
-onesideofsquare = 2
-print onesideofsquare
-lengthofrectangle = 4
-print lengthofrectangle
-heightofrectangle = 5
-print heightofrectangle
-oneyearinmonths = 12
-myage = 16
-myageinmonths = oneyearinmonths * myage
-print myageinmonths
-averagefemalelifeexpectancy = 71
-myage =16
-yearstolive = averagefemalelifeexpectancy - myage
-print  yearstolive
-onemeterinfeet = 3.28084
-myheightinfeet = myheightinmeters * onemeterinfeet
-print myheightinfeet
-
-averagefemaleheightthailand = 157.3
-myheight = 162
-differenceofmyheightandaverageheight = myheight - averagefemaleheightthailand
-print differenceofmyheightandaverageheight
-areaofsquare = onesideofsquare * onesideofsquare
-print areaofsquare
-volumeofhalfacube = (onesideofsquare ** 3 )/2
-print volumeofhalfacube
-oneninethareaofrectangle = (heightofrectangle * lengthofrectangle) * (1.0/9)
-print oneninethareaofrectangle
+myname = "Sandra" #defined my name
+myageindecimals = 16.9 #defined my age in decimals
+myheightinmeters = 1.62 #defined my height in meters
+onesideofsquare = 2 #define one side of a square
+lengthofrectangle = 4 #defined one length of a rectangle
+heightofrectangle = 5 #defined the height of the rectangle
+oneyearinmonths = 12 #defined the number of months in a year
+myage = 16 #defined my age
+myageinmonths = oneyearinmonths * myage #calculated my age in months using two codes
+averagefemalelifeexpectancy = 71 #defined the average female life expectancy 
+myage =16 #defined my age
+yearstolive = averagefemalelifeexpectancy - myage #calculated the number of years i had left to live
+onemeterinfeet = 3.28084 #defined how many feet are in meters
+myheightinfeet = myheightinmeters * onemeterinfeet #calculated my height in feet
+averagefemaleheightthailand = 157.3 #defined the average female height in Thailand. 
+myheight = 162 #defined my height
+differenceofmyheightandaverageheight = myheight - averagefemaleheightthailand #calculated the difference between my height and the average female height in Thailand. 
+areaofsquare = onesideofsquare * onesideofsquare #calculated the area of a square with a previous defined side. 
+volumeofhalfacube = (onesideofsquare ** 3 )/2 #calculated the volume of half a cube
+oneninethareaofrectangle = (heightofrectangle * lengthofrectangle) * (1.0/9) #calculated one ninth of the area of a rectangle 
 print "Hello, people." + "My name is" + " " + myname + "." + "I am" + " " + str(myage) + "years old." + "I am" + " " + str(myageinmonths) + " " + "months old." + "I have approximately" + " " + str(yearstolive) + " " + "years to live." + "I am" + " " + str(myheight) + " " + "centimeters tall."
 print "Hi there","i have a square and a rectangle.","The square has a side of",str(onesideofsquare),"units.","Its area is",str(areaofsquare),"units squared.","The volume of half the square is",str(volumeofhalfacube),"units cubed.","The rectangle has a lenth of",str(lengthofrectangle),"and height of",str(heightofrectangle),"units."
 winkyface = ";)" 
