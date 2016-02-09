27603d23a2116d42d16ba4c55cec7dccd95fe208
cmis <cmis@cmis-hs09.(none)>
2016-02-09 14:00:40 +0700

    assignment function

---
+++ functions.py
@@ -0,0 +1,33 @@
+def add(a, b):
+	return a+b
+
+c= add(3,4)
+print c
+
+def sub(a, b):
+	return a-b
+
+d= sub(5,3)
+print d
+
+def mul(a,b):
+	return a*b
+
+e= mul(4, 4)
+print e
+
+def div(a,b):
+	return a/b
+
+f= div(2.0,3)
+print f
+
+def hours_from_seconds(a,b):
+	return a/b
+
+g= hours_from_seconds(86400, 3600)
+print g
+
+def circle_area(r):
+	return 3.14159265359 * (r**2)
+print circle_area(5)
