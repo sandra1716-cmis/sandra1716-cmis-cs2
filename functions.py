824a5dc4c5df71e575a5b9227e223e252430b681
cmis <cmis@cmis-hs09.(none)>
2016-02-11 09:51:20 +0700

    defined more functions

--- functions.py
+++ functions.py
@@ -1,33 +1,43 @@
-def add(a, b):
+def add(a, b): #defined a function with variables through additon 
 	return a+b
 
 c= add(3,4)
 print c
 
-def sub(a, b):
+def sub(a, b): #defined a function with variables through subtraction
 	return a-b
 
 d= sub(5,3)
 print d
 
-def mul(a,b):
+def mul(a,b): #defined a function with variables through multiplication 
 	return a*b
 
 e= mul(4, 4)
 print e
 
-def div(a,b):
+def div(a,b): #defined a function with variables through division
 	return a/b
 
 f= div(2.0,3)
 print f
 
-def hours_from_seconds(a,b):
+def hours_from_seconds(a,b): #defined a function by changing seconds to hours
 	return a/b
 
 g= hours_from_seconds(86400, 3600)
 print g
 
-def circle_area(r):
-	return 3.14159265359 * (r**2)
+import math
+def circle_area(r): #defined a function by returning an area of a circle
+	return math.pi * (r**2)
 print circle_area(5)
+
+import math
+def sphere_volume (a): #defined a function by representing an argument and changing it into the volume
+	return 1.33333333333 * math.pi * (a**3)
+print sphere_volume (5)
+
+def avg_volume (a,b):
+	return 
+h= avg_volume (10,20)
