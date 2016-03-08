4ccb5085ea3cdd9db1e9ce36aba829268ddfb980
cmis <cmis@cmis-hs09.(none)>
2016-03-08 09:40:37 +0700

    edited format

--- structure.py
+++ structure.py
@@ -1,46 +1,30 @@
 import math
 
-BMR(Basal Meabolic Rate) this formula uses your gender, height, weight, & age to calculate your Basal Metabolic Rate. This calculates the amount of calories you need based on your body weight. 
 
-Women BMR = 655 + (4.35*wp) + (4.7*hi) - (4.7*age)
-BMR = Basal Metabolic Rate
-wp = weight_in_pounds
-hi = height_in_inches
-age = age_in_years
 
 def BMR (wight,height,age):
 	bmr = 655 + (4.35*weight_pounds) + (4.7*height_inches) - (4.7*age_years)
 	return bmr
 
 
-def output (name,weight_pounds, height_inches, age_years, bmi):
-	return = """
+def output (name,weight_pounds, height_inches, age_years, bmr):
+	return """
 
 Hey {}!
 Did you know your BMR is:
 655 + (4.35*{}) + (4.7*{}) - (4.7*{}) = {}
-""".format (name,weight_pounds, height_inches, age_years, bmr):
+""".format (name,weight_pounds, height_inches, age_years, bmr)
 
 
 def main():
 	name = raw_input ("What's your name?:")
 	weight_pounds = raw_input ("Type your weight in pounds:")
 	height_inches= raw_input ("Type your height in inches:")
-	age_years= raw_input ("Type your age in years")
+	age_years= raw_input ("Type your age in years:")
 	bmr = BMR(float(weight_pounds), float(height_inches), float(age_years))
-	print output(name,weight_pounds, height_inches, age_years, bmi)
+	print output(name,weight_pounds, height_inches, age_years, bmr)
 
 main()
-
-
-z = add(int(x), int(y))
-
-out = output(name,x,y,z)
-print out
-
-
-
-
 
 
 
