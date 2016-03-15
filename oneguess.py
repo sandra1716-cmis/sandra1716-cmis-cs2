5317ddc70786edcdc3cb6e0479ddaa4c8ad4489f
cmis <cmis@cmis-hs09.(none)>
2016-03-15 09:51:32 +0700

    worked on guess game

--- oneguess.py
+++ oneguess.py
@@ -1,1 +1,10 @@
-#
+def guessminnumber
+	number =int(raw_input("Type the min number(0-99):"))
+
+def main():
+	minnumber = raw_input("What is the minimum number?:")
+	maxnumber = raw_input("What is the maximum number?:")
+	thinking = raw_input("Im thinking of a number:")
+	guess = raw_input ("What od you think it is?:")
+	
+main()
