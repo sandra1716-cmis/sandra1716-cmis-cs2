1864644c280cc2a7da606b4773ee1f9b32805fd7
cmis <cmis@cmis-hs19.(none)>
2016-03-22 09:55:06 +0700

    worked on

--- oneguess.py
+++ oneguess.py
@@ -1,70 +1,56 @@
 import random
 
-def guessdifference(target,guess):
-	guessdif= target-guess
-	return guessdif
 
 
-def outputtemplate (minnumber,yourguess,maxnumber):
-	return"""
+
+def outputtemplate (random1,yourguess, result):
+	outputtemplate_1="""
 The target was {}.
 Your guess was {}.
 That's under by {}.
-""".format(minnumber,yourguess,maxnumber)
+""".format(random1,yourguess, result)
+	return outputtemplate_1
 
+def outputtemplate2 (random1, yourguess, result):
+	outputtemplate_2="""
+The target was {}.
+Your guess was {}.
+That's over by {}.
+""".format(random1,yourguess,result)
+	return outputtemplate_2
 
+def outputtemplate3 (random1, yourguess):
+	outputtemplate_3="""
+The target was {}.
+Your guess was {}.
+That's correct!!!
+""".format(random1,yourguess)
+	return outputtemplate_3
 
 def main():
-	minnumber = raw_input("What is the minimum number?:")
-	maxnumber = raw_input("What is the maximum number?:")
+	minnumber = int(raw_input("What is the minimum number?:"))
+	maxnumber = int(raw_input("What is the maximum number?:"))
 	output = """ The number is between {} and {}""".format(minnumber, maxnumber) 
+	random1 = random.randint (minnumber, maxnumber)
 	print output 
-	yourguess= raw_input("What do you think it is?:")
-	print outputtemplate(minnumber,yourguess,maxnumber)
+	yourguess= int(raw_input("What do you think it is?:"))
+	result =abs(yourguess-random1)
 
+
+	if random1>abs(yourguess):
+		outputtemplate_1 =outputtemplate (random1,yourguess,result)
+		print outputtemplate_1
+	elif random < abs(yourguess):
+		outputtemplate_2 = outputtemplate2(random1, yourguess, result)
+		print outputtemplate_2
+	elif random1 == abs(yourguess):
+		outputtemplate_3 =outputtemplate3(random1, yourguess)
+		print outputtemplate_3
 main()
 
 
-import random
-
-def main(): 
-	minimum = raw_input("Type in the minimum number:")
-	maximum = raw_input("Type in the maximum number:")
-
-	output = """ I'm thinking of a number between {} and {} """.format(minimum, maximum)
-	print output
 
 
-	number = random.randint(int(minimum), int(maximum))
-	your_number = raw_input("Guess the number:")
-	output = """ The number was {} """.format(number)
-	print output	
-	output = """ Your guess was {} """.format(your_number) 	
-	
-	if your_number == number:
-		return True
-		output = " Congratulations, you guessed the number correctly"		
-		print output
-
-
-	if your_number > number:
-		difference = int(your_number) -  int(number)
-		
-		output = """ Thats {} higher than the number """.format(difference)
-		print output	
-	
-	if your_number < number:
-		more_than = int(number) - int(number)
-		
-		output = """ Your guess was under by {}  """.format(more_than)
-		print output 
-
-	
-			
-	
-
-
-main()
 
 
 
