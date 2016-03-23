ea7035725c973d4016bdba943055f682b429ff42
cmis <cmis@cmis-hs19.(none)>
2016-03-23 11:27:27 +0700

    called the function correctly

--- structure.py
+++ structure.py
@@ -3,7 +3,7 @@ import math
 
 
 def BMR (weight,height,age):
-	bmr = 655 + (4.35*weight_pounds) + (4.7*height_inches) - (4.7*age_years)
+	bmr = 655 + (4.35*weight) + (4.7*height) - (4.7*age)
 	return bmr
 
 
@@ -18,10 +18,10 @@ Did you know your BMR is:
 
 def main():
 	name = raw_input ("What's your name?:")
-	weight_pounds = raw_input ("Type your weight in pounds:")
-	height_inches= raw_input ("Type your height in inches:")
-	age_years= raw_input ("Type your age in years:")
-	bmr = 655 + (4.35*int(weight_pounds)) + (4.7*int(height_inches)) - (4.7*int(age_years))
+	weight_pounds = int(raw_input ("Type your weight in pounds:"))
+	height_inches= int(raw_input ("Type your height in inches:"))
+	age_years= int(raw_input ("Type your age in years:"))
+	bmr=BMR(weight_pounds, height_inches,age_years)
 	print output(name,weight_pounds, height_inches, age_years, bmr)
 
 main()
