3dd2c347373f21d50ab4aa39369bd62c12c8bee2
cmis <cmis@cmis-hs19.(none)>
2016-03-23 11:51:54 +0700

    wrote the description for the script

---
+++ conditionals.py
@@ -0,0 +1,5 @@
+#This script will a game where you have to figure out what to wear depending on the weather.
+#The two options are summer clothes or a raincoat.
+#summer clothes will be worn on a sunny day but the raincoat will be worn if it's raining. 
+
+
