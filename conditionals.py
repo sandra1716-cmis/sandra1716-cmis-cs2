c4c1eaf5bf841732ca10a9c345598acb5428b79c
cmis <cmis@cmis-hs19.(none)>
2016-03-30 11:16:00 +0700

    worked

--- conditionals.py
+++ conditionals.py
@@ -7,27 +7,36 @@ import random
 def hunger (decision):
 	
 	if decision == "yes":
-		print "The choices are"
-		foodchoice =raw_input ("Would you like to have cheeze pizza or lasagna?:")
+		print "Welcome to the World Famous Italian Restaurant!!"
+		foodchoice =raw_input ("Would you like to have cheeze pizza or lasagna to eat?: ")
 		food(foodchoice)
-	elif decision =="No":
-		print "Thank you for coming, have a nice day!"
+	elif decision =="no":
+		print "Goodbye, have a nice day!"
 
 def food(decision2):
 	if decision2 == "cheeze pizza":
 		print "Your cheeze pizza will be prepared in 15 min."
 	elif decision2 == "lasagna":
 		print "Your lasagna will be prepared in 20 min." 
+	
+		
+def drink (drinkoption):
+	if drinkoption == "yes":
+		print "We have water and coke which one would you like?"
+	elif drinkoption == "no":
+		print "Alright if that's all here's your check."
 
 
 
 def main():
-	decision1 = raw_input("Are you hungry?:")
+	
+	decision1 = raw_input("Are you hungry?: ")
 	
 	hunger (decision1)
 	
+	decision3 = raw_input ("Would u like something to drink?: ")
 
-	
+	drink (decision3)
 main()
 
 
