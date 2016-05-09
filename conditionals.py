a79371d5716fd4c7b54620c11f866eda26fcb604
cmis <cmis@cmis-hs19.(none)>
2016-05-09 14:30:10 +0700

    added return value

--- conditionals.py
+++ conditionals.py
@@ -7,25 +7,28 @@ import random
 def hunger (decision):
 	
 	if decision == "yes":
+		
 		print "Welcome to the World Famous Italian Restaurant!!"
 		foodchoice =raw_input ("Would you like to have cheeze pizza or lasagna to eat?: ")
 		food(foodchoice)
+		return "yes"
 	elif decision =="no":
 		print "Goodbye, have a nice day!"
+		return "no"	
 
 def food(decision2):
 	if decision2 == "cheeze pizza":
 		print "Your cheeze pizza will be prepared in 15 min."
+		return int(20)
 	elif decision2 == "lasagna":
 		print "Your lasagna will be prepared in 20 min." 
-	
+		return int(25)
 		
 def drink (drinkoption):
 	if drinkoption == "yes":
 		print "We have water and coke which one would you like?"
 	elif drinkoption == "no":
 		print "Alright if that's all here's your check."
-
 
 
 def main():
