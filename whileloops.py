9cd04a8c378bc29f81b4ef3f9dac6b471a41450a
cmis <cmis@cmis-hs19.(none)>
2016-05-30 09:54:08 +0700

    grid

--- whileloops.py
+++ whileloops.py
@@ -9,5 +9,31 @@ def counter(x):
 	while x<= 0:
 		print x
 		x+=1
-	while 
 	
+	
+def sumOfOdds(n):
+	total =0
+	if n>0:
+		while n>0:
+			if n% 2 ==1:
+				total += n
+			n -= 1
+	elif n<0:
+		while n<0:
+			if n% 2==1:
+				total +=n
+			n+= 1
+	return total
+
+print sumOfOdds(10)
+
+
+def grid (w,h):
+	out =""
+	while w>0:
+		out += "."
+		w-= 1
+	return out
+	
+
+print grid (10,10)
